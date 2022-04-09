12a7e23a2aa9e993bec7554b9738f2a4a102bd4a
卷毛 <47733889+zoengzihaam@users.noreply.github.com>
2022-04-09 15:06:23 +0800

    Update pack_firmware.sh
    modify KERNEL_VERSION_NAME

--- pack_firmware.sh
+++ pack_firmware.sh
@@ -2,7 +2,7 @@
 
 OPENWRT_ARMVIRT="$(pwd)/bin/tmp/openwrt-armvirt-64-default-rootfs.tar.gz"
 PACKAGE_SOC="s905d"
-KERNEL_VERSION_NAME="5.4.142"
+KERNEL_VERSION_NAME="5.4.188"
 OPENWRT_VER="immortal21.02"
 WHOAMI="ziznah"
 
